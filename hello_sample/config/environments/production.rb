53b1565e7d8e73a9a19329bfad3956b3f4911626
Noishiki Tatsunori <t.noishiki0201@gmail.com>
2019-12-23 20:36:45 +0900

    change crud

--- hello_sample/config/environments/production.rb
+++ hello_sample/config/environments/production.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "hello_sample_production"
+  # config.active_job.queue_name_prefix = "Hello_App_production"
 
   config.action_mailer.perform_caching = false
 
